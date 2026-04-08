56f1cdd85cfcf87eb80c92261ce7407e53c29e0e
Batdrew1233 <drewstevenson1233@gmail.com>
2026-04-08 10:05:14 -0400

    Initial Commit

--- src/main/java/com/plurasight/MathApp.java
+++ src/main/java/com/plurasight/MathApp.java
@@ -1,4 +1,11 @@
 package com.plurasight;
 
 public class MathApp {
+    public static void main(String[] args) {
+        // Calculating Highest Salary
+        int bobSalary = 52854;
+        int garySalary = 62812;
+        int highestSalary = Math.max(bobSalary,garySalary);
+        System.out.println("The highest salary is $" + highestSalary );
+    }
 }
